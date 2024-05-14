b723c457394862ecd2fa6ffdfac28b7d50af4204
NHnew nabi <nbihuseynov1@gmail.com>
2024-05-14 23:00:41 +0000

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PageContainer from './container/PageContainer';
 import Header from './components/Header';
 import RouterConfig from './config/RouterConfig';
 import Loading from './components/Loading';
+import { useLocation } from 'react-router-dom';
 
 const App = () => {
+
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+
   return (
     <div>
       <Header />
