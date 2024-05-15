9c7c4221ce5df9821ea781ac01c1becb200f9773
NHnew nabi <nbihuseynov1@gmail.com>
2024-05-15 12:01:26 +0000

    ch

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const ProductDetails = () => {
+
+
     return (
-        <div className='mt-[80px]'>ProductDetails</div>
+        <div className='mt-[80px]'>
+
+            Product-Details
+
+        </div>
     );
 };
 
